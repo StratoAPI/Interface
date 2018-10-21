09ba79d52b4969addd14ca46db21b973b7bddec6
Vilsol <me@vil.so>
2018-10-21 23:20:16 +0300

    Add generic resource fetcher

--- resource/types.go
+++ resource/types.go
@@ -1,11 +1,17 @@
 package resource
 
-import "github.com/StratoAPI/Interface/plugins"
+import (
+	"github.com/StratoAPI/Interface/filter"
+	"github.com/StratoAPI/Interface/plugins"
+)
 
 type Processor interface {
 	// Get the store of a resource
 	GetStore(resource string) *plugins.Storage
 
 	// Get a list of all resources
-	GetResources() []string
+	GetResourceList() []string
+
+	// Retrieve resources.
+	GetResources(resource string, filters []filter.ProcessedFilter) ([]map[string]interface{}, error)
 }
